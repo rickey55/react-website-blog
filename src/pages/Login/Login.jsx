9c87054a4267be3d5022cd16d1dd2606ada7d067
Molly <momolly1024@gmail.com>
2021-09-15 09:54:25 +0800

    react-router

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,16 +1,30 @@
-import React from "react";
+import { Link } from "react-router-dom";
 import "./login.css";
+
 export default function Login() {
     return (
         <div className="login">
+            <span className="loginTitle">Login</span>
             <form className="loginForm">
                 <label>Email</label>
-                <input type="text" placeholder="enter your email" />
-                <label>password</label>
-                <input type="password" placeholder="enter your password" />
+                <input
+                    className="loginInput"
+                    type="text"
+                    placeholder="Enter your email..."
+                />
+                <label>Password</label>
+                <input
+                    className="loginInput"
+                    type="password"
+                    placeholder="Enter your password..."
+                />
+                <button className="loginButton">Login</button>
             </form>
-            <button className="loginButton">Login</button>
-            <button className="loginRegisterButton">Register</button>
+            <button className="loginRegisterButton">
+                <Link className="link" to="/register">
+                    Register
+                </Link>
+            </button>
         </div>
     );
 }
